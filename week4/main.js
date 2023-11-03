b9e5f2bb9d6ed1ec3fa0a8d2f50470b59ec71905
Ishwarya-GowriSankar <145094234+Ishwarya-GowriSankar@users.noreply.github.com>
2023-11-03 09:57:44 +0400

    commit main

--- week4/main.js
+++ week4/main.js
@@ -26,7 +26,7 @@ var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
+ourRequest.open('GET', 'https://Ishwarya-GowriSankar.github.io/Ishwarya-GowriSankar.github.io/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 renderHTML(ourData);
