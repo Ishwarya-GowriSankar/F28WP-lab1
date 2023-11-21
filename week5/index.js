a1b69063cad08ffd7f2183ba33ee0232a4414231
Ishwarya-GowriSankar <145094234+Ishwarya-GowriSankar@users.noreply.github.com>
2023-11-21 23:19:56 +0400

    second commit

--- week5/index.js
+++ week5/index.js
@@ -1,5 +1,20 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port=5000;
+const db = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'user_auth'
+})
+db.connect((error)=>{
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log('Mysql Connected...')
+    }
+});
 app.get("/",(req, res ) =>{ res.send ("<h1>HOMEPAGE</h1>")});
 app.listen (port, () => {console.log (`Server started on port ${port}`);})
