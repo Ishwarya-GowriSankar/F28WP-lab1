0b77b31fb70bd997a8898138a9cfcbee848690e3
Ishwarya-GowriSankar <145094234+Ishwarya-GowriSankar@users.noreply.github.com>
2023-11-22 12:58:01 +0400

    Part 2 of video

--- week5/index.js
+++ week5/index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config({path:'./.env'});
+const path = require('path');
 const app = express();
 const port=5000;
 const db = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '',
-    database : 'user_auth'
+    host : process.env.DATABASE_HOST,
+    user :  process.env.DATABASE_USER,
+    password :  process.env.DATABASE_PASSWORD,
+    database : process.env.DATABASE
 })
 db.connect((error)=>{
     if(error){
@@ -16,5 +19,13 @@ db.connect((error)=>{
         console.log('Mysql Connected...')
     }
 });
-app.get("/",(req, res ) =>{ res.send ("<h1>HOMEPAGE</h1>")});
+const publicDirectory = path.join(__dirname, './public');
+app.set('view engine', 'hbs');
+app.use(express.static(publicDirectory));
+
+
+app.get("/",(req, res ) =>{ 
+    //res.send ("<h1>HOMEPAGE</h1>")
+    res.render("index");
+});
 app.listen (port, () => {console.log (`Server started on port ${port}`);})
